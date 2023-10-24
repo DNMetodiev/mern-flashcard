ce427788652390322b6df0d58a2081e9eb161fff
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-10-25 00:27:26 +0300

    set mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,13 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import Deck from "./models/Deck";
+
+const PORT = 5000;
+
 const app = express();
+
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("gg!");
@@ -10,4 +18,10 @@ app.get("/hello", (req: Request, res: Response) => {
 });
 
 
-app.listen(5000);
+const db = mongoose.connect(
+  "mongodb+srv://flashcardsage:KIJSJUwHLUGO6yT9@cluster0.dqjq9vg.mongodb.net/?retryWrites=true&w=majority"
+).then(() => {
+  console.log(`Connected to ${PORT}`);
+  app.listen(PORT);
+});
+
