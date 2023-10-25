acdb7c019fb43192ef393bc8a2fe579ebd874c60
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-10-26 02:18:55 +0300

    adding dotenv library for security

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
+import { config } from "dotenv";
+config();
+
 import Deck from "./models/Deck";
 
 const PORT = 5000;
@@ -22,9 +25,7 @@ app.post("/decks", async (req: Request, res: Response) => {
 });
 
 
-const db = mongoose.connect(
-  "mongodb+srv://flashcardsage:KIJSJUwHLUGO6yT9@cluster0.dqjq9vg.mongodb.net/?retryWrites=true&w=majority"
-).then(() => {
+const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`Connected to ${PORT}`);
   app.listen(PORT);
 });
