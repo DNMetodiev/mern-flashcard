719715906ccceae09f8cc8e5faa239401e647e1b
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-11-08 18:59:29 +0200

    Add DELETE endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,6 +31,13 @@ app.post("/decks", async (req: Request, res: Response) => {
   res.json(createdDeck);
 });
 
+app.delete("/decks/:deckId", async (req: Request, res: Response) => {
+  //TODO - get deck ID from the url, delete the deck from the database, return the deleted deck to the user who made the reques
+  const deckId = req.params.deckId;
+  const deck = await Deck.findByIdAndDelete(deckId);
+  res.json(deck);
+});
+
 
 const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`Connected to ${PORT}`);
